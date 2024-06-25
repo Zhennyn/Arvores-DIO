10c14f476d50d07ab99c86a1a492b9a627991fbb
Zhennyn <matheuslimamenezes2005@gmail.com>
2024-06-25 15:47:56 -0300

    first commit

---
+++ src/one/digitalinnovation/Main.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation;
+
+import one.digitalinnovation.model.Obj;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        ArvoreBinaria<Obj> minhArvore = new ArvoreBinaria<>();
+        
+        minhArvore.inserir(new Obj(5));
+        minhArvore.inserir(new Obj(21));
+        minhArvore.inserir(new Obj(12));
+        minhArvore.inserir(new Obj(40));
+        minhArvore.inserir(new Obj(37));
+        minhArvore.inserir(new Obj(14));
+
+        minhArvore.exibirInOrnem();
+        minhArvore.exibirPreOrdem();
+        minhArvore.exibirPosOrdem();
+        minhArvore.remover(21);
+
+
+    }
+}
